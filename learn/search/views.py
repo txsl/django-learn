8e44afb638f5520aa7e82e29b6e220a4461c8000
Thomas Lim <txl11@ic.ac.uk>
2013-04-01 20:45:45 +0100

    Less stuff to deal with request

--- learn/search/views.py
+++ learn/search/views.py
@@ -1,12 +1,15 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader, Context
+#from django.http import HttpResponse
+#from django.template import loader, Context
 from django.contrib.flatpages.models import FlatPage
+from django.shortcuts import render_to_response
 
 def search(request):
 	query = request.GET['q']
-	results = FlatPage.objects.filter(content__icontains=query)
-	template = loader.get_template('search/search.html')
-	context = Context({'query': query, 'results': results})
-	response = template.render(context)
-	return HttpResponse(response)
+	#results = FlatPage.objects.filter(content__icontains=query)
+	#template = loader.get_template('search/search.html')
+	#context = Context({'query': query, 'results': results})
+	#response = template.render(context)
+	return render_to_response('search/search.html', { 'query' : query,
+														'results' : FlatPage.objects.filter(content__icontains=query)
+															})
